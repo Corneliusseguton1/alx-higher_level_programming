370b5c220c6939b53d15f2e0ad4340e067a41786
Cornelius <segutoncornelius@gmail.com>
2023-10-20 09:36:41 +0000

    update

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Module for Base class.'''
+
+class Base
+	'''A representation of the base of our oop hiererchy.'''
+
+	_nb_objects = 0
+
+	def _init_(self, id=none):
+		'''Constructor.'''
+		if id is not None:
+			self.id = id
+		else
+			Base._nb_objects += 1
+			self.id = Base._nb_objects
