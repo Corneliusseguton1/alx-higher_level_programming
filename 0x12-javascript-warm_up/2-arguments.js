3b7a2d3a4e8c87f3bf501d27cb174ba422a35de9
Cornelius <segutoncornelius@gmail.com>
2024-02-13 07:58:38 -0800

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-if (process.agrv.length === 2) (
-	console.log("No argument")
-)
-else if (process.argv.length === 3) (
-	console.log("Argument found")
-)
-else (
-	console.log("Argument found")
-)
+const len = process.argv.length;
+if (len <= 2) {
+	  console.log('No argument');
+} else if (len === 3) {
+	  console.log('Argument found');
+} else {
+	  console.log('Arguments found');
+}
