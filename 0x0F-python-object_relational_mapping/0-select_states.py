c624ce38b34e542bb1e7518ad5ff44a5e65b044f
Cornelius <segutoncornelius@gmail.com>
2024-02-17 12:46:26 -0800

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,9 @@
-#!/usr/bin/python3
-"""List states Module"""
-import MySQLdb
-from sys import argv
-
-
-if __name__ == "__main__":
-        db = MySQLdb.connect(
-                            host='localhost',
-                                        port=3306,
-                                                    user=argv[1],
-                                                                passwd=argv[2],
-                                                                            db=argv[3])
-
-            cursor = db.cursor()
-                cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-                    rows = cursor.fetchall()
-                        for row in rows:
-                                    print(row)
-                                        cursor.close()
-                                            db.close()
+-- Create states table in hbtn_0e_0_usa with some data
+CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+USE hbtn_0e_0_usa;
+CREATE TABLE IF NOT EXISTS states ( 
+                id INT NOT NULL AUTO_INCREMENT, 
+                        name VARCHAR(256) NOT NULL,
+                                PRIMARY KEY (id)
+                                );
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
